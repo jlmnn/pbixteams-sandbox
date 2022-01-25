cc10e7037d954d0c52e8d5032668a7db6068dae7
Christina Juulmann <cjuulmann@microsoft.com>
2022-01-25 08:49:25 +0100

    added MS Sandbox scripts

---
+++ shared/getapps.cmd
@@ -0,0 +1,5 @@
+REM Download Teams app
+curl -L "https://go.microsoft.com/fwlink/p/?LinkID=869426&clcid=0x809&culture=en-gb&country=GB&lm=deeplink&lmsrc=groupChatMarketingPageWeb&cmpid=directDownloadWin64" --output C:\Users\cj\teams.exe
+
+REM install and run Teams
+C:\Users\cj\teams.exe /verysilent /suppressmsgboxes
