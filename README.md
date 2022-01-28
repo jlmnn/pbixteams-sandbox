ecfd9b2b02e33dccf0955d895c067923b3078ce6
Christina Juulmann <cjuulmann@microsoft.com>
2022-01-28 07:55:53 +0100

    updated readme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # pbixteams-sandbox
 Sandbox demo environment with Power BI and Teams
+
+## Prerequisite
+
+* Microsoft Sandbox to be enabled in the Windows Features panel
+<img src="img/windowsfeatures-winsandbox.png">
+
+
+* Microsoft Teams module (PowerShell) 
+For more information on how to install the Teams module in Power Shell visit https://docs.microsoft.com/en-us/microsoftteams/teams-powershell-install
--- shared/getapps.cmd
+++ shared/getapps.cmd
@@ -1,5 +1,8 @@
-REM Download Teams app
+:: download power bi desktop
+:: install and run pbi
+
+REM download teams app
 curl -L "https://go.microsoft.com/fwlink/p/?LinkID=869426&clcid=0x809&culture=en-gb&country=GB&lm=deeplink&lmsrc=groupChatMarketingPageWeb&cmpid=directDownloadWin64" --output C:\Users\cj\teams.exe
 
-REM install and run Teams
+REM install and run teams
 C:\Users\cj\teams.exe /verysilent /suppressmsgboxes
